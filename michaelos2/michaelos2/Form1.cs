e986f4f53b201313d4b09f576edf4ffac4fc5c83
landofisos <72436036+landofisos@users.noreply.github.com>
2021-03-23 12:09:09 +0100

    michael
    michelle skelpat

---
+++ michaelos2/michaelos2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace michaelos2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            Form2 form2 = new Form2();
+            if (progressBar1.Value != 100)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                timer1.Stop();
+                form2.Show();
+            }
+        }
+    }
+}
---
+++ michaelos2/michaelos2/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace michaelos2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Form1 obj = (Form1)Application.OpenForms["Form1"];
+            obj.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser childForm = new browser(); //initialize a child form
+
+            childForm.TopLevel = false; //set it's TopLevel to false
+
+            Controls.Add(childForm); //and add it to the parent Form
+            childForm.Show(); //finally display it
+
+            childForm.BringToFront();
+        }
+    }
+}
